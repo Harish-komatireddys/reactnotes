0aa1cfa4ad9166246af4d25a2ffac589dd49765b
Harish-komatireddys <34508376+Harish-komatireddys@users.noreply.github.com>
2021-10-11 11:26:44 +0530

    Updated COde

---
+++ src/pages/Note.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from 'react';
+import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg';
+
+const Note = ({match, history}) => {
+    const noteId = match.params.id;
+
+    const [note, setNote] = useState(null)
+
+    useEffect(() => {
+        getNote()
+    }, [noteId])
+
+    let getNote = async () => {
+        if (noteId === 'new') return
+        let response = await fetch(`/api/notes/${noteId}`)
+        let data = await response.json()
+        setNote(data)
+    }
+
+    let updateNote = async () => {
+        fetch(`/api/notes/${noteId}/update/`, {
+            method : "PUT",
+            headers : {
+                "Content-Type" : "application/json"
+            },
+            body : JSON.stringify(note)
+        })
+    }
+
+    let deleteNote = async () => {
+        fetch(`/api/notes/${noteId}/delete/`, {
+            method : 'DELETE',
+            headers: {
+                "Content-Type" :  "application/json"
+            }
+        })
+        history.push("/")
+    }
+
+    let createNote = async () => {
+        fetch('/api/notes/create/', {
+            method : "POST",
+            headers : {
+                "Content-Type" : "application/json"
+            },
+            body : JSON.stringify(note)
+        })
+        history.push('/')
+    }
+
+    let handleSubmit = () => {
+        if(noteId === 'new' && note.body === ''){
+            deleteNote()
+        }else if(noteId !== 'new' && note.body !== ''){
+            updateNote()
+        }else if(noteId === 'new' && note.body !== ''){
+            createNote()
+        }else if(note.body === ''){
+            deleteNote()
+        }
+        history.push("/")
+    }
+
+    return (
+        <div className="note">
+            <div className="note-header">                
+                <h3>
+                    <ArrowLeft onClick={handleSubmit}/>
+                </h3>  
+                {noteId !== 'new' ? (
+                    <button onClick={deleteNote}>Delete</button> 
+                ) : (
+                    <button onClick={createNote}>Done</button> 
+                )}
+                             
+            </div>
+            <textarea onChange={(e) => {setNote({...note, 'body': e.target.value})}} defaultValue={note?.body}></textarea>
+        </div>
+    );
+}
+
+export default Note;
---
+++ src/pages/NotesList.js
@@ -0,0 +1,38 @@
+import React, {useEffect, useState} from 'react';
+import Listitem from '../components/Listitem';
+import { ReactComponent as AddIcon } from '../assets/add.svg';
+import { Link } from 'react-router-dom';
+
+const NotesList = () => {
+    let [notes, setNotes] = useState([]);
+
+    useEffect(() => {
+        getNotes()
+    }, [])
+
+    let getNotes = async () => {
+        let response = await fetch('/api/notes/');
+        let data = await response.json();
+        //console.log(data);
+        setNotes(data);
+    }
+
+    return (
+        <div className="notes">
+            <div className="note-header">
+                <h2 className="note-title">&#9782; Notes</h2>
+                <p className="note-count">{notes.length}</p>
+            </div>        
+            <div className="notes-list">
+                { notes.map((note, index) => (
+                    <Listitem key={index} note={note}/>
+                )) }
+            </div>
+            <Link to='/note/new' className="floating-button">
+                <AddIcon />
+            </Link>
+        </div>
+    );
+}
+
+export default NotesList;
